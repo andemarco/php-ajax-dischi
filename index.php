90d867e1877e4827add95c46d6a8d0d46b555b1c
Andrea <andreademarco91@gmail.com>
2020-02-25 16:01:23 +0100

    LAYOUT OK

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <nav class="nav_bar">
+        <img src="https://developer.spotify.com/assets/branding-guidelines/icon3@2x.png" alt="Spotify LOGO">
+      </nav>
+    </header>
+    <main>
+      <div class="d-flex container">
+        <div class="box">
+          <img src="https://www.ondarock.it/images/cover/makai_252x252_1535803175.jpg" alt="">
+          <h5>Comfort Zone</h5>
+          <h6>Makai</h6>
+          <p>2018</p>
+        </div>
+      </div>
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>
